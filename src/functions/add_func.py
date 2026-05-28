5106411d0c0a7e067797396e49fad324cb5cf4a5
sjentoft <susiejentoft@gmail.com>
2026-05-28 13:49:54 +0200

    Removed branch from import and added test function

---
+++ src/functions/add_func.py
@@ -0,0 +1,16 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# %%
+def add(x,y):
+    x+y
